4c15995a5c9d9cc48632ddfd9e5807379a58c825
Richard Mr <richard@yegobox.com>
2019-08-25 14:02:59 +0200

    is loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+
+import { TranslateService } from "@ngx-translate/core";
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'evaluate';
+  constructor(private translate: TranslateService,public router: Router) {
+    translate.setDefaultLang("en");
+   
+  }
+ 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
-export const environment = {
-  production: true
+export const AppConfig = {
+  production: true,
+  environment: "PROD",
+  url: "https://flipper.yegobox.rw/"
 };
